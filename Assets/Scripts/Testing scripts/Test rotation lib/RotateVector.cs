f5417e09b46a2f171a694425485965e442a4c1e0
alex brah <qetting@gmail.com>
2021-02-08 23:49:23 +0200

    implemented rotate vector class

--- Assets/Scripts/Testing scripts/Test rotation lib/RotateVector.cs
+++ Assets/Scripts/Testing scripts/Test rotation lib/RotateVector.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 
 public enum RotationDirection
 {
+    None,
     Clockwise,
     CounterClockwise
 }
 
 public static class RotateVector
 {
-   public static Vector2 Rotate(Vector2 vector, RotationDirection rotation)
+    public static Vector2 Rotate(Vector2 vector, RotationDirection rotation)
     {
-        throw new System.NotImplementedException();
+        Vector2 rotated;
+        switch (rotation)
+        {
+            case RotationDirection.None:
+                rotated = vector;
+                break;
+            case RotationDirection.Clockwise:
+                rotated = RotateClockwise(vector);
+                break;
+            case RotationDirection.CounterClockwise:
+                rotated = RotateCounterClockwise(vector);
+                break;
+            default:
+                throw new UnityException($"RotationDirection type<{rotation}> not supported!");
+        }
+        return rotated;
     }
+    private static Vector2 RotateClockwise(Vector2 v)
+    {
+        return new Vector2(v.y, -v.x);
+    }
+    private static Vector2 RotateCounterClockwise(Vector2 v)
+    {
+        return new Vector2(-v.y, v.x);
+    }
+
 }
